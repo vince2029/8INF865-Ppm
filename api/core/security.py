041babe9909078faacbb01c30d368d166fcd93e7
ArnoBidet <arno.bidet479@gmail.com>
2026-03-26 10:59:12 -0400

    Add OAuth2 password bearer and implement user ID retrieval from JWT

--- api/core/security.py
+++ api/core/security.py
@@ -3,11 +3,14 @@ import bcrypt
 from datetime import datetime, timedelta
 from typing import Optional
 from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
 
 # Configuration
 SECRET_KEY = os.getenv("SECRET_KEY", "le-secret-des-mille-pattes-2026")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 1440 # 24 heures
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def hash_password(password: str) -> str:
     # On transforme le str en bytes pour bcrypt
@@ -30,3 +33,14 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user_id(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Token invalide")
+        return user_id
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token invalide ou expiré")
